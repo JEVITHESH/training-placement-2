bf20de560add468f2bf2f8e7c3ee0ddf22262a25
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-06-21 20:19:56 +0530

    Create Print Elements of Array Using For-Each.java

---
+++ 21-06-2025/Print Elements of Array Using For-Each.java
@@ -0,0 +1,10 @@
+
+
+public class ArrayPrint {
+    public static void main(StringPrint Elements of Array Using For-Each[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+}
